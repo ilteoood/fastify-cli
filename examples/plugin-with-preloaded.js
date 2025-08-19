5df262cd2afb840e22c3942eb30c390fb216c1de
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-08-19 17:59:29 +0200

    chore: plugin with preloaded

--- examples/plugin-with-preloaded.js
+++ examples/plugin-with-preloaded.js
@@ -1,13 +1,13 @@
 /* global GLOBAL_MODULE_1, GLOBAL_MODULE_3 */
 'use strict'
-const t = require('tap')
+const { ok } = require('node:assert')
 
 module.exports = async function (fastify, options) {
   fastify.get('/', async function (req, reply) {
     return { hasPreloaded: GLOBAL_MODULE_1 && GLOBAL_MODULE_3 }
   })
   fastify.addHook('onReady', function () {
-    t.ok(GLOBAL_MODULE_1)
-    t.ok(GLOBAL_MODULE_3)
+    ok(GLOBAL_MODULE_1)
+    ok(GLOBAL_MODULE_3)
   })
 }
